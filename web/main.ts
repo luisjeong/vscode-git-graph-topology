b4c39b2665c76a24b75898f93b61c898a4f53ee8
정용기 <ykjeong.ssafy@gmail.com>
2026-05-13 17:59:18 +0900

    feat: remove legacy commit ordering menu

--- web/main.ts
+++ web/main.ts
@@ -622,7 +622,7 @@ class GitGraphView {
 			showRemoteBranches: getShowRemoteBranches(repoState.showRemoteBranchesV2),
 			includeCommitsMentionedByReflogs: getIncludeCommitsMentionedByReflogs(repoState.includeCommitsMentionedByReflogs),
 			onlyFollowFirstParent: getOnlyFollowFirstParent(repoState.onlyFollowFirstParent),
-			commitOrdering: getCommitOrdering(repoState.commitOrdering),
+			commitOrdering: GG.CommitOrdering.Date,
 			remotes: this.gitRemotes,
 			hideRemotes: repoState.hideRemotes,
 			stashes: this.gitStashes
@@ -1823,12 +1823,6 @@ class GitGraphView {
 				this.render();
 			};
 
-			const commitOrdering = getCommitOrdering(this.gitRepos[this.currentRepo].commitOrdering);
-			const changeCommitOrdering = (repoCommitOrdering: GG.RepoCommitOrdering) => {
-				this.saveRepoStateValue(this.currentRepo, 'commitOrdering', repoCommitOrdering);
-				this.refresh(true);
-			};
-
 			contextMenu.show([
 				[
 					{
@@ -1848,26 +1842,6 @@ class GitGraphView {
 						visible: true,
 						checked: columnWidths[4] !== COLUMN_HIDDEN,
 						onClick: () => toggleColumnState(4, 80)
-					}
-				],
-				[
-					{
-						title: 'Commit Timestamp Order',
-						visible: true,
-						checked: commitOrdering === GG.CommitOrdering.Date,
-						onClick: () => changeCommitOrdering(GG.RepoCommitOrdering.Date)
-					},
-					{
-						title: 'Author Timestamp Order',
-						visible: true,
-						checked: commitOrdering === GG.CommitOrdering.AuthorDate,
-						onClick: () => changeCommitOrdering(GG.RepoCommitOrdering.AuthorDate)
-					},
-					{
-						title: 'Topological Order',
-						visible: true,
-						checked: commitOrdering === GG.CommitOrdering.Topological,
-						onClick: () => changeCommitOrdering(GG.RepoCommitOrdering.Topological)
 					}
 				]
 			], true, null, e, this.viewElem);
@@ -3751,21 +3725,6 @@ function getChildByPathSegment(folder: FileTreeFolder, pathSeg: string) {
 	return cur;
 }
 
-
-/* Repository State Helpers */
-
-function getCommitOrdering(repoValue: GG.RepoCommitOrdering): GG.CommitOrdering {
-	switch (repoValue) {
-		case GG.RepoCommitOrdering.Default:
-			return initialState.config.commitOrdering;
-		case GG.RepoCommitOrdering.Date:
-			return GG.CommitOrdering.Date;
-		case GG.RepoCommitOrdering.AuthorDate:
-			return GG.CommitOrdering.AuthorDate;
-		case GG.RepoCommitOrdering.Topological:
-			return GG.CommitOrdering.Topological;
-	}
-}
 
 function getShowRemoteBranches(repoValue: GG.BooleanOverride) {
 	return repoValue === GG.BooleanOverride.Default
